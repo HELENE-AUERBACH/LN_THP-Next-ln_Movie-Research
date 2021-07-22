2c856eeb1192a224a01069134fa6bcdee04f21dc
HELENE-AUERBACH <ln_au@yahoo.fr>
2021-07-22 17:16:01 +0200

    Suppression of the "form" HTML tag and simplification.

--- js/custom.js
+++ js/custom.js
@@ -2,32 +2,14 @@ const keyWords = document.getElementById("keyWords");
 const loadButton = document.getElementById("load-button");
 const designDiv = document.getElementById("design-div");
 
-async function fetchWithTimeout(resource, options) {
-  const { timeout = 8000 } = options;
-  const controller = new AbortController();
-  const id = setTimeout(() => controller.abort(), timeout);
-  const response = await fetch(resource, {
-    ...options,
-    signal: controller.signal
-  });
-  clearTimeout(id);
-  return response;
-}
-
 async function loadMovies() {
   try {
-    let delay = setTimeout(() => {
-      log(`Loading movies... for key words : ${keyWords.value}`);
-    }, 6000);
-    const response = await fetchWithTimeout(`http://www.omdbapi.com/?s=${encodeURIComponent(keyWords.value)}&apikey=ce0e87a6`, {
-      timeout: 6000
-    });
+    const response = await fetch(`http://www.omdbapi.com/?s=${encodeURIComponent(keyWords.value)}&apikey=ce0e87a6`);
     //console.log(response);
-    clearTimeout(delay);
     if (response.ok) {
       const responseJson = await response.json();
-      const posts = JSON.stringify(responseJson);
-      console.log(`posts: ${posts}`);
+      //const posts = JSON.stringify(responseJson);
+      //console.log(`posts: ${posts}`);
       return responseJson;
     } else {
       console.log("Not successful");
@@ -39,14 +21,21 @@ async function loadMovies() {
 }
 
 loadButton.addEventListener("click", () => {
+  let delay = setTimeout(() => {
+    log(`Loading movies... for key words : ${keyWords.value}`);
+  }, 600);
   loadMovies().then(function(res) {
     designDiv.innerHTML = "";
+    clearTimeout(delay);
     if (res !== null && res !== undefined) {
       //console.log(`res: ${JSON.stringify(res)}`);
       const results = res.Search;
       const totalResults = results.length;
-      log(`${totalResults} Movie${totalResults > 1 ? "s have to " : " has to "} be loaded.`);
       //console.log(`Fetched results: ${JSON.stringify(results)}`);
+      delay = setTimeout(() => {
+	log(`${totalResults} Movie${totalResults > 1 ? "s have to " : " has to "} be loaded.`);
+      }, 600);
+      clearTimeout(delay);
       results.map((result) => searchMovie(designDiv, result.Poster, result.Title, result.Year, result.imdbID, result.Type));
       log(`${totalResults} Movie${totalResults > 1 ? "s have " : " has "} been loaded.`);
     }
@@ -57,22 +46,31 @@ function log(message) {
   document.getElementById("message").innerText = message;
 }
 
-async function searchMovie(selector, URL, title, year, imdbID, imdbType) {
+function searchMovie(selector, URL, title, year, imdbID, imdbType) {
+  if (imdbID !== null && imdbID !== undefined) {
+    let delay = setTimeout(() => {
+      log(`Loading movie... for Id : ${imdbID}`);
+    }, 600);
+    loadMovie(imdbID).then(function(res) {
+      clearTimeout(delay);
+      if (res !== null && res !== undefined) {
+        //console.log(`res: ${JSON.stringify(res)}`);
+        showMovie(designDiv, res.Poster, res.Title, res.Released, res.Plot, res.imdbID, res.Type);
+      }
+    });
+  }
+}
+
+async function loadMovie(imdbID) {
   if (imdbID !== null && imdbID !== undefined) {
     try {
-      let delay = setTimeout(() => {
-        log(`Loading movie... for Id : ${imdbID}`);
-      }, 6000);
-      const response = await fetchWithTimeout(`http://www.omdbapi.com/?i=${imdbID}&apikey=ce0e87a6`, {
-        timeout: 6000
-      });
+      const response = await fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=ce0e87a6`);
       //console.log(response);
-      clearTimeout(delay);
       if (response.ok) {
         const responseJson = await response.json();
-        const post = JSON.stringify(responseJson);
-        console.log(`post: ${post}`);
-        responseJson.then(showMovie(designDiv, responseJson.Poster, responseJson.Title, responseJson.Released, responseJson.Plot, responseJson.imdbID, responseJson.Type));
+        //const post = JSON.stringify(responseJson);
+        //console.log(`post: ${post}`);
+        return responseJson;
       } else {
         console.log("Not successful");
       }
